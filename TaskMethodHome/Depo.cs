a4090cdb3eb10e95df03285f6c8d066479a76829
[shaigak] <shaigak@code.edu.az>
2022-12-19 23:53:28 +0300

    New

---
+++ TaskMethodHome/Depo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMethodHome
+{
+    internal class Depo
+    {
+    }
+}
